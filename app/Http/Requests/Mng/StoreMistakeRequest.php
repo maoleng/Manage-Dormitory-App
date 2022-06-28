d62c772648f20381e628eea6e99ef021965e8ad8
Bùi Hữu Lộc <feature451@gmail.com>
2022-06-28 19:07:37 +0700

    move path

---
+++ app/Http/Requests/Mng/StoreMistakeRequest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Requests\Mng;
+
+use App\Models\Student;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+use JetBrains\PhpStorm\ArrayShape;
+
+class StoreMistakeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    #[ArrayShape(['student_card_id' => "string[]", 'content' => "string[]"])]
+    public function rules(): array
+    {
+        return [
+            'student_card_id' => [
+                'required',
+                Rule::exists(Student::class, 'student_card_id'),
+            ],
+            'content' => [
+                'required'
+            ]
+        ];
+    }
+
+    #[ArrayShape(['required' => "string", 'exists' => "string"])]
+    public function messages(): array
+    {
+        return [
+            'required' => "Chưa nhập :attribute",
+            'exists' =>  ":attribute không tồn tại"
+        ];
+    }
+
+    #[ArrayShape(['student_card_id' => "string", 'content' => "string"])]
+    public function attributes(): array
+    {
+        return [
+            'student_card_id' => "mã thẻ sinh viên",
+            'content' => "nội dung vi phạm"
+        ];
+    }
+
+    public function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(response()->json([
+            'status'   => false,
+            'message'   => 'Lỗi dữ liệu truyền lên',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
