44d9571b64e6bbe9a1ed474f8acfba15da66569d
Bùi Hữu Lộc <feature451@gmail.com>
2022-07-09 22:55:24 +0700

    Update FormController.php

--- app/Http/Controllers/Std/FormController.php
+++ app/Http/Controllers/Std/FormController.php
@@ -37,7 +37,41 @@ class FormController extends Controller
             ];
         }
 
-        return ((new \App\Http\Controllers\Mng\FormController)->showConversation($id));
+        $form = Form::query()
+            ->with('student')
+            ->with('images')
+            ->with('childAnswer.student')
+            ->with('childAnswer.teacher')
+            ->with('childAnswer.images')
+            ->find($id);
+        if (empty($form)) {
+            return [
+                'status' => false,
+                'message' => 'Không tìm thấy đơn'
+            ];
+        }
+
+        $data = [];
+        $data['id'] = $form->id;
+        $data['student'] = $form->student;
+        $data['title'] = $form->title;
+        $data['content'] = $form->content;
+        $data['created_at'] = $form->created_at;
+        $data['images'] = $form->images;
+
+        foreach ($form->childAnswer as $key => $each_answer) {
+            $data['answers'][$key]['id'] = $each_answer->id;
+            $data['answers'][$key]['student'] = $each_answer->student;
+            $data['answers'][$key]['teacher'] = $each_answer->teacher;
+            $data['answers'][$key]['content'] = $each_answer->content;
+            $data['answers'][$key]['created_at'] = $each_answer->created_at;
+            $data['answers'][$key]['images'] = $each_answer->images;
+        }
+
+        return [
+            'status' => true,
+            'data' => $data
+        ];
 
     }
 
