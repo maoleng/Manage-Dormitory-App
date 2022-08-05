144a90a8158fe3162be5852329d483d454b9dbc4
Bùi Hữu Lộc <feature451@gmail.com>
2022-08-05 20:45:45 +0700

    Update MistakeFactory.php

--- database/factories/MistakeFactory.php
+++ database/factories/MistakeFactory.php
@@ -19,11 +19,14 @@ class MistakeFactory extends Factory
     public function definition()
     {
         $random_type = $this->faker->numberBetween(1, 10);
+        $random_bool = $this->faker->numberBetween(0, 1);
         return [
             'student_id' => Contract::query()->whereNotNull('subscription_id')->inRandomOrder()->value('student_id'),
             'teacher_id' => Teacher::query()->inRandomOrder()->value('id'),
             'type' => $random_type,
             'content' => $random_type === 10 ? $this->faker->sentence($nbWords = 6, $variableNbWords = true) : null,
+            'is_fix_mistake' => $random_bool,
+            'is_confirm' => $random_bool === 1 ? 1 : 0,
             'date' => $this->faker->dateTime($max = 'now', $timezone = null)
         ];
     }
