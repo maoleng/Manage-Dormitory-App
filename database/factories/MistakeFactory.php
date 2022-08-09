193ab1bc624424fabec3d98a2ba0107694e63834
Bùi Hữu Lộc <feature451@gmail.com>
2022-08-09 12:15:27 +0700

    fix time mistake seeder

--- database/factories/MistakeFactory.php
+++ database/factories/MistakeFactory.php
@@ -20,6 +20,7 @@ class MistakeFactory extends Factory
     {
         $random_type = $this->faker->numberBetween(1, 10);
         $random_bool = $this->faker->numberBetween(0, 1);
+        $random_date = $this->faker->randomElement([2021, 2022, 2022, 2022, 2022]) . '-' . $this->faker->date($format = 'm-d', $max = 'now') . ' ' . $this->faker->time($format = 'H:i:s', $max = 'now');
         return [
             'student_id' => Contract::query()->whereNotNull('subscription_id')->inRandomOrder()->value('student_id'),
             'teacher_id' => Teacher::query()->inRandomOrder()->value('id'),
@@ -27,7 +28,7 @@ class MistakeFactory extends Factory
             'content' => $random_type === 10 ? $this->faker->sentence($nbWords = 6, $variableNbWords = true) : null,
             'is_fix_mistake' => $random_bool,
             'is_confirmed' => $random_bool === 1 ? 1 : 0,
-            'date' => $this->faker->dateTime($max = 'now', $timezone = null)
+            'date' => $random_date
         ];
     }
 }
